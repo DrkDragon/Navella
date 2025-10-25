4743f98f1fc84fbcb782ee84bb4b8ae4f1694137
Lucida Dragon <lucidadragon@gmail.com>
2025-10-25 14:22:10 -0400

    Added rulebook button

--- src/main.gd
+++ src/main.gd
@@ -1,4 +1,7 @@
 extends Control
 
 func _on_mute_button_pressed() -> void:
-	$AudioStreamPlayer.stream_paused = $MuteButton.button_pressed
+	$AudioStreamPlayer.stream_paused = $Buttons/MuteButton.button_pressed
+
+func _on_rules_button_pressed() -> void:
+	OS.shell_open("https://raw.githubusercontent.com/DrkDragon/Navella/refs/heads/main/Navella.pdf")
